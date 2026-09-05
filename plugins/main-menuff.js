51a347f257038d1fa52a10d0c454ed3f81bb2463
elmamonero <samilamie8@gmail.com>
2026-09-05 14:04:05 -0400

    Update message sending logic in main-menuff.js
    Refactor message sending to use image with caption instead of text with external ad reply.

--- plugins/main-menuff.js
+++ plugins/main-menuff.js
@@ -51,18 +51,9 @@ let handler = async (m, { conn, usedPrefix }) => {
 
     let img = 'https://evogb.win/Menu';  
 
-    conn.sendMessage(m.chat, {  
-        text: ff,  
-        contextInfo: {  
-            externalAdReply: {  
-                title: '𓂂𓏸  𐅹੭੭ ᴍᴇɴᴜ ᴅᴇ ғʀᴇᴇ ғɪʀᴇ 🌙 ᦡᦡ',  
-                body: dev,  
-                thumbnailUrl: img,  
-                sourceUrl: redes,  
-                mediaType: 1,  
-                renderLargerThumbnail: true  
-            }  
-        }  
+    await conn.sendMessage(m.chat, {  
+        image: { url: img },  
+        caption: ff  
     }, { quoted: fkontak });  
     
     m.react('🔫');  
@@ -72,4 +63,4 @@ handler.help = ['menuff (Menu Free Fire)'];
 handler.tag = ['crow'];  
 handler.command = ['menuff'];  
 
-export default handler;  
+export default handler;
